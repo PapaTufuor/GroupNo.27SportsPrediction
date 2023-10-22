d70f70345a69001fb4d2988c8a656eff85870481
PapaTufuor <111927122+PapaTufuor@users.noreply.github.com>
2023-10-22 19:33:36 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -17,16 +17,29 @@ with open('C:\\Users\\hp\\Desktop\\Mid Sem Project\\top_features_list.pkl','rb')
 # print(type(model))
 
 
+feature_name_mapping ={
+    "Value in Euros":top_features_list[0],
+    "Age":top_features_list[1],
+    "Release Clause": top_features_list[2],
+    "Movement Reactions": top_features_list[3],
+    "Potential": top_features_list[4],
+    "Goalkeeping": top_features_list[5],
+    "Wages": top_features_list[6]
+}
 
 st.title('FIFA Player Rating Predictor')
 st.sidebar.header('Enter Feature Values')
 
+
+
+
+
 user_inputs={}
-for feature in top_features_list:
-    user_inputs[feature] = st.sidebar.number_input(f'Enter value for {feature}', value=0.0)
+for user_friendly_name, actual_name in feature_name_mapping.items():
+    user_inputs[user_friendly_name] = st.sidebar.number_input(f'Enter value for {user_friendly_name}', value=0.0)
 
 if st.sidebar.button('Predict'):
-    input_data=pd.DataFrame(user_inputs, index=[0])
+    input_data=pd.DataFrame({actual_name: user_inputs[user_friendly_name] for user_friendly_name, actual_name in feature_name_mapping.items()}, index=[0])
 
     scaled_input_data=scaler.transform(input_data)
 
@@ -37,10 +50,10 @@ if st.sidebar.button('Predict'):
 
     max_possible_rating=100.0
 
-    if predicted_rating[0] ==max_possible_rating:
-        confidence=1.0
-    else:
-        confidence=1.0 -abs(predicted_rating[0] - max_possible_rating)/max_possible_rating
+    deviation=abs(predicted_rating[0] - max_possible_rating)
+    confidence=1.0 - (deviation/max_possible_rating)
+
+    
 
     st.write(f'Predicted Rating: {predicted_rating[0]:.2f}')
     st.write(f'Confidence Level: {confidence * 100:.2f}%')
